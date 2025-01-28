ebb0e9566e2d29a8a8515ae553b433c57e7e25bd
Anubhav choudhary <uditnousran@gmail.com>
2025-01-28 23:15:44 +0530

    adding tailwind code

---
+++ tailwind/1/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='grid grid-cols-1 gap-x-8 gap-y-4 md:grid-cols-2 lg:grid-cols-4 '>
+<div className='bg-red-300  md:bg-red-600'>1st div</div>
+<div className='bg-yellow-200  '>2nd div</div>
+<div className='bg-pink-300 '>3rd div</div>
+<div className='bg-green-600 '>4th div</div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
